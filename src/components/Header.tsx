5e3e748d118f9460fcc4aa199ba0f88eb3813e73
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-03 16:38:43 +0100

    styled header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,67 @@ import React from 'react';
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5'>
+        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+          <h1 className='text-xl font-semibold whitespace-nowrap'>
+            Tasty Recipes
+          </h1>
+          <div className='flex items-center lg:order-2'>
+            <a
+              href='/'
+              className='text-gray-800 hover:bg-gray-50 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2'
+            >
+              Log in
+            </a>
+            <a
+              href='/'
+              className='text-white bg-primary-700 hover:bg-primary-800 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2'
+            >
+              Get started
+            </a>
+          </div>
+          <div className='hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1'>
+            <ul className='flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0'>
+              <li>
+                <a
+                  href='/'
+                  className='block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0'
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/'
+                  className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/'
+                  className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
+                >
+                  Recipes
+                </a>
+              </li>
+              <li>
+                <a
+                  href='/'
+                  className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
