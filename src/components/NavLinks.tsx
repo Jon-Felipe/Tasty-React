fa4023eb309acaf875c5bdbf55cfbb4fd16813e0
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-03 19:03:05 +0100

    made header links dynamic

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { navLinks } from '../utils/constants';
 
 type Props = {};
 
@@ -6,38 +7,19 @@ const NavLinks = (props: Props) => {
   return (
     <div className='hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1'>
       <ul className='flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0'>
-        <li>
-          <a
-            href='/'
-            className='block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0'
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href='/'
-            className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href='/'
-            className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
-          >
-            Recipes
-          </a>
-        </li>
-        <li>
-          <a
-            href='/'
-            className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
-          >
-            Contact
-          </a>
-        </li>
+        {navLinks.map((link) => {
+          const { id, text, path } = link;
+          return (
+            <li key={id}>
+              <a
+                href={path}
+                className='block py-2 pr-4 pl-3 text-gray-700 capitalize border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
+              >
+                {text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 1,
+    text: 'home',
+    path: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    path: '/about',
+  },
+  {
+    id: 3,
+    text: 'recipes',
+    path: '/recipes',
+  },
+  {
+    id: 4,
+    text: 'contact',
+    path: '/contact',
+  },
+];
