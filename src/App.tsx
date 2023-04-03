6e8ec780679938af628622940814386551f99085
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-03 19:54:17 +0100

    created home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import React, { useState } from 'react';
 // components
 import Header from './components/Header';
 
+// pages
+import Home from './pages/Home';
+
 function App() {
   const [showLinks, setShowLinks] = useState<boolean>(false);
 
   return (
     <>
       <Header showLinks={showLinks} setShowLinks={setShowLinks} />
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
