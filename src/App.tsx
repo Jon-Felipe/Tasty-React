8f2cfc2468e58bd7e0e80543c9366ef0738fab56
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-03 16:21:22 +0100

    created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// components
+import Header from './components/Header';
+
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
