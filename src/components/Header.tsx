210724ce6ee15ac5f717b5650e645e9d9296e873
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-04 09:27:06 +0100

    updated header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,9 +14,9 @@ type Props = {
 const Header = ({ showLinks, setShowLinks }: Props) => {
   return (
     <header>
-      <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5'>
+      <nav className='bg-white px-4 lg:px-6 py-2 lg:py-4 shadow'>
         <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
-          <h1 className='text-xl font-semibold whitespace-nowrap'>
+          <h1 className='text-xl lg:text-3xl font-semibold whitespace-nowrap'>
             Tasty Recipes
           </h1>
           <div className='flex items-center lg:order-2'>
