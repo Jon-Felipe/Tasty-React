0cc041ec6f2f56850805618d9e1a384be7e092b3
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-04 09:46:00 +0100

    updated html semantics

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ function App() {
   return (
     <>
       <Header showLinks={showLinks} setShowLinks={setShowLinks} />
-      <main>
+      <>
         <Home />
-      </main>
+      </>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,26 +6,28 @@ type Props = {};
 const Home = (props: Props) => {
   return (
     <section className='px-4 lg:px-6 pt-8'>
-      <div className='max-w-screen-xl mx-auto'>
+      <main className='max-w-screen-xl mx-auto'>
         {/* hero section */}
-        <div className='h-52 lg:h-96 rounded-xl overflow-hidden'>
+        <section className='h-52 lg:h-96 rounded-xl overflow-hidden'>
           <img
             src={heroImg}
             alt='hero image'
             className='w-full h-full object-cover'
           />
-        </div>
+        </section>
 
-        {/* intro text */}
-        <h1 className='text-4xl text-red-500 font-bold uppercase'>Recipes</h1>
-        <p className='mt-4'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-          aliquid distinctio tempora, culpa commodi ipsa consequatur voluptas
-          repellat officia eligendi maxime porro aspernatur sapiente in unde ad,
-          dolorum doloremque temporibus ea quam! Repellendus mollitia placeat
-          laboriosam qui, provident assumenda. Vero.
-        </p>
-      </div>
+        {/* intro text section */}
+        <section>
+          <h1 className='text-4xl text-red-500 font-bold uppercase'>Recipes</h1>
+          <p className='mt-4'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+            aliquid distinctio tempora, culpa commodi ipsa consequatur voluptas
+            repellat officia eligendi maxime porro aspernatur sapiente in unde
+            ad, dolorum doloremque temporibus ea quam! Repellendus mollitia
+            placeat laboriosam qui, provident assumenda. Vero.
+          </p>
+        </section>
+      </main>
     </section>
   );
 };
