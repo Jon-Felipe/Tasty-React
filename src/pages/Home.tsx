277182bd0edbbc70d7505d668cc7917e6489c917
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-04 09:45:18 +0100

    added hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import heroImg from '../assets/hero-img.jpg';
 
 type Props = {};
 
@@ -6,6 +7,16 @@ const Home = (props: Props) => {
   return (
     <section className='px-4 lg:px-6 pt-8'>
       <div className='max-w-screen-xl mx-auto'>
+        {/* hero section */}
+        <div className='h-52 lg:h-96 rounded-xl overflow-hidden'>
+          <img
+            src={heroImg}
+            alt='hero image'
+            className='w-full h-full object-cover'
+          />
+        </div>
+
+        {/* intro text */}
         <h1 className='text-4xl text-red-500 font-bold uppercase'>Recipes</h1>
         <p className='mt-4'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
