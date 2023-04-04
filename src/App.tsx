07a687391b5f8ea9b03b6ca3349c7a42a1743af6
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-04 09:29:50 +0100

    updated home screen styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Header showLinks={showLinks} setShowLinks={setShowLinks} />
-      <main className='px-4 lg:px-6 py-2.5'>
+      <main>
         <Home />
       </main>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,17 @@ type Props = {};
 
 const Home = (props: Props) => {
   return (
-    <section className='max-w-screen-xl mx-auto'>
-      <h1 className='text-4xl text-red-500 font-bold uppercase'>Recipes</h1>
-      <p className='mt-4'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid
-        distinctio tempora, culpa commodi ipsa consequatur voluptas repellat
-        officia eligendi maxime porro aspernatur sapiente in unde ad, dolorum
-        doloremque temporibus ea quam! Repellendus mollitia placeat laboriosam
-        qui, provident assumenda. Vero.
-      </p>
+    <section className='px-4 lg:px-6 pt-8'>
+      <div className='max-w-screen-xl mx-auto'>
+        <h1 className='text-4xl text-red-500 font-bold uppercase'>Recipes</h1>
+        <p className='mt-4'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+          aliquid distinctio tempora, culpa commodi ipsa consequatur voluptas
+          repellat officia eligendi maxime porro aspernatur sapiente in unde ad,
+          dolorum doloremque temporibus ea quam! Repellendus mollitia placeat
+          laboriosam qui, provident assumenda. Vero.
+        </p>
+      </div>
     </section>
   );
 };
