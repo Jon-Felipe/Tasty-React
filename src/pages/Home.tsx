f28b7a66653d9b05caadf19528e82b564558e6be
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-04 09:48:33 +0100

    updated home page html semantics

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,19 @@ type Props = {};
 
 const Home = (props: Props) => {
   return (
-    <section className='px-4 lg:px-6 pt-8'>
-      <main className='max-w-screen-xl mx-auto'>
+    <main className='px-4 lg:px-6 pt-8'>
+      <section className='max-w-screen-xl mx-auto'>
         {/* hero section */}
-        <section className='h-52 lg:h-96 rounded-xl overflow-hidden'>
+        <article className='h-52 lg:h-96 rounded-xl overflow-hidden'>
           <img
             src={heroImg}
             alt='hero image'
             className='w-full h-full object-cover'
           />
-        </section>
+        </article>
 
         {/* intro text section */}
-        <section>
+        <article>
           <h1 className='text-4xl text-red-500 font-bold uppercase'>Recipes</h1>
           <p className='mt-4'>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
@@ -26,9 +26,9 @@ const Home = (props: Props) => {
             ad, dolorum doloremque temporibus ea quam! Repellendus mollitia
             placeat laboriosam qui, provident assumenda. Vero.
           </p>
-        </section>
-      </main>
-    </section>
+        </article>
+      </section>
+    </main>
   );
 };
 
