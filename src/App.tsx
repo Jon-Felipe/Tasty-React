08df78b06035226b10a4409a4d90998f17eae09a
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-05 21:06:13 +0100

    create recipes page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Main from './layouts/Main';
 // pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Recipes from './pages/Recipes';
 import Contact from './pages/Contact';
 
 const router = createBrowserRouter([
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: 'about',
         element: <About />,
+      },
+      {
+        path: 'recipes',
+        element: <Recipes />,
       },
       {
         path: 'contact',
---
+++ src/pages/Recipes.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Recipes = (props: Props) => {
+  return <div>Recipes</div>;
+};
+
+export default Recipes;
