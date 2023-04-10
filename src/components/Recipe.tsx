7f20cf7fd06e90c3cf9f6d9fe082108b7124fbe6
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-10 19:57:42 +0100

    added dummy recipe data

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import foodImg from '../assets/hero-img.jpg';
 
-type Props = {};
+type Props = {
+  text: string;
+  details: object;
+  image: string;
+  rating: number;
+  ratingAmt: number;
+};
 
-const Recipe = (props: Props) => {
-  return <div>Recipe</div>;
+const Recipe = ({ text, details, image, rating, ratingAmt }: Props) => {
+  return (
+    <article>
+      <img src={foodImg} alt={text} />
+      <div>
+        <h3>{text}</h3>
+        <div>
+          <p>{rating}</p>
+          <span>{ratingAmt}</span>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Recipe;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -59,7 +59,16 @@ const Home = (props: Props) => {
         <h3 className='text-2xl font-semibold mb-4'>Explore Recipes</h3>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
           {recipes.map((recipe: RecipeType) => {
-            return <Recipe key={recipe.id} />;
+            return (
+              <Recipe
+                key={recipe.id}
+                text={recipe.text}
+                details={recipe.recipe_details}
+                image={recipe.image}
+                rating={recipe.averate_rating}
+                ratingAmt={recipe.ratings}
+              />
+            );
           })}
         </div>
       </article>
