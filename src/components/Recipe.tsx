ae3674a1f70653728fb22ca76662f5a431e46a49
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-11 20:30:10 +0100

    updated button text

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -43,7 +43,7 @@ const Recipe = ({ text, image, ratingAmt, reviews, description }: Props) => {
           type='button'
           className='block w-full text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mt-3'
         >
-          Get Recipe
+          Get The Recipe
         </button>
       </section>
     </article>
