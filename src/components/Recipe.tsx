cf31b4797f432d89b14e2b05e59b7e820636a776
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-11 20:06:39 +0100

    added button to recipe component

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -44,6 +44,12 @@ const Recipe = ({
         <div>
           <p>{description}</p>
         </div>
+        <button
+          type='button'
+          className='text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mt-3'
+        >
+          Get Recipe
+        </button>
       </article>
     </article>
   );
