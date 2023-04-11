a79415309a4fd750f42c8f85a6a4aebad258fc64
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-11 20:16:53 +0100

    updated layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,7 +57,7 @@ const Home = (props: Props) => {
       {/* explore recipes */}
       <article>
         <h3 className='text-2xl font-semibold mb-4'>Explore Recipes</h3>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
           {recipes.map((recipe: RecipeType) => {
             return (
               <Recipe
