09e7b724b15b8a9f79a493e8f989e2293cd8f30e
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-11 20:44:42 +0100

    added favourite icon

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  HeartIcon,
   HandThumbUpIcon,
   ChatBubbleOvalLeftEllipsisIcon,
 } from '@heroicons/react/24/outline';
@@ -18,8 +19,13 @@ const Recipe = ({ text, image, ratingAmt, reviews, description }: Props) => {
     <article className='shadow rounded-xl overflow-hidden'>
       <img src={foodImg} alt={text} />
       <section className='py-2 lg:py-4 px-4 lg:-x-8'>
-        <h3 className='text-3xl font-semibold'>{text}</h3>
-        <div className='flex items-center gap-3'>
+        <header className='flex items-center justify-between'>
+          <h3 className='text-3xl font-semibold'>{text}</h3>
+          <button type='button'>
+            <HeartIcon className='h-8 w-8 text-red-500' />
+          </button>
+        </header>
+        <section className='flex items-center gap-3'>
           <div className='flex gap-2'>
             <HandThumbUpIcon className='h-5 w-6' />
             <p>
@@ -32,7 +38,7 @@ const Recipe = ({ text, image, ratingAmt, reviews, description }: Props) => {
               {reviews} <span className='font-semibold text-sm'>Comments</span>
             </p>
           </div>
-        </div>
+        </section>
         <div className='py-2'>
           <p>{description}</p>
         </div>
