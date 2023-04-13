4634ab7115544aa50c7db756cec296abd84573e0
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-13 19:59:35 +0100

    updated page styling

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -9,11 +9,11 @@ type Props = {};
 
 const Recipes = (props: Props) => {
   return (
-    <article>
-      <h3 className='text-5xl font-semibold mb-12 underline'>
+    <article className='max-w-screen-xl mx-auto grid gap-6'>
+      <h3 className='text-5xl font-semibold mb-6 underline'>
         Explore Our Tasty Recipes
       </h3>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {recipes.map((recipe: RecipeType) => {
           return (
             <Recipe
