3e2c7de77e355015c6b363e83462aff88fca5803
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-13 19:24:28 +0100

    added recipe component to recipe page

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { recipes } from '../utils/constants';
+import Recipe from '../components/Recipe';
 
 type Props = {};
 
 const Recipes = (props: Props) => {
-  return <div>Recipes</div>;
+  return (
+    <article>
+      <h3>Explore Recipes</h3>
+      {recipes.map((recipe) => {
+        return (
+          <Recipe
+            key={recipe.id}
+            text={recipe.text}
+            image={recipe.image}
+            ratingAmt={recipe.ratings}
+            reviews={recipe.reviews}
+            description={recipe.description}
+          />
+        );
+      })}
+    </article>
+  );
 };
 
 export default Recipes;
