f5bedd49405a2a70d5603efea624ace208130cf4
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-13 19:20:51 +0100

    update nav link to Link component

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {
   text: string;
@@ -8,12 +9,12 @@ type Props = {
 const NavLink = ({ text, path }: Props) => {
   return (
     <li>
-      <a
-        href={path}
+      <Link
+        to={path}
         className='block py-2 pr-4 pl-3 text-gray-700 capitalize border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
       >
         {text}
-      </a>
+      </Link>
     </li>
   );
 };
