f4fa37cfcd4c1abe1616fd1ecc38d604a825427f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-15 10:57:22 +0100

    added default form label and input

--- src/components/FormRow.tsx
+++ src/components/FormRow.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 type Props = {};
 
 const FormRow = (props: Props) => {
-  return <div>FormRow</div>;
+  return (
+    <div>
+      <label htmlFor=''></label>
+      <input type='text' />
+    </div>
+  );
 };
 
 export default FormRow;
