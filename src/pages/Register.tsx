832ee09098ff9b3a94b9d29fd6af45d72423fd3c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-15 10:51:51 +0100

    create register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,93 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {};
 
 const Register = (props: Props) => {
-  return <div>Register</div>;
+  return (
+    <section>
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <Link
+          to='/'
+          className='flex items-center mb-6 text-2xl font-semibold text-gray-900'
+        >
+          <h1>Tasty Recipes</h1>
+        </Link>
+        <div className='w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 '>
+          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>
+              Create an account
+            </h1>
+            <form className='space-y-4 md:space-y-6'>
+              <div>
+                <label
+                  htmlFor='email'
+                  className='block mb-2 text-sm font-medium text-gray-900'
+                >
+                  Your email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 '
+                  placeholder='name@company.com'
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor='password'
+                  className='block mb-2 text-sm font-medium text-gray-900'
+                >
+                  Your password
+                </label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 '
+                  placeholder='••••••••'
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor='password'
+                  className='block mb-2 text-sm font-medium text-gray-900'
+                >
+                  Confirm password
+                </label>
+                <input
+                  type='password'
+                  name='confirmPassword'
+                  id='confirmPassword'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 '
+                  placeholder='••••••••'
+                  required
+                />
+              </div>
+              <button
+                type='submit'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+              >
+                Create an account
+              </button>
+              <p className='text-sm font-light text-gray-500'>
+                Already have an account?{' '}
+                <Link
+                  to='/login'
+                  className='font-medium text-primary-600 hover:underline'
+                >
+                  Login here
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
