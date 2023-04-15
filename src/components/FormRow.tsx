9f1114a1e60d10f7ceb319244910f7cc9fa7c446
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-15 11:02:12 +0100

    added form row props and types

--- src/components/FormRow.tsx
+++ src/components/FormRow.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+  type: React.HTMLInputTypeAttribute | undefined;
+  name: string | undefined;
+  value: string | number | readonly string[] | undefined;
+  handleChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  labelText: string | undefined;
+};
 
-const FormRow = (props: Props) => {
+const FormRow = ({ type, name, value, handleChange, labelText }: Props) => {
   return (
     <div>
-      <label htmlFor=''></label>
-      <input type='text' />
+      <label htmlFor={name}>{labelText || name}</label>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   );
 };
