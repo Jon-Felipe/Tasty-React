e0a89667be182473ad519b9cf0fdf8624eacc184
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-16 19:13:19 +0100

    removed button

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -42,12 +42,6 @@ const Recipe = ({ text, image, ratingAmt, reviews, description }: Props) => {
         <div className='py-2'>
           <p>{description}</p>
         </div>
-        <button
-          type='button'
-          className='block w-full text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mt-3'
-        >
-          Get The Recipe
-        </button>
       </section>
     </article>
   );
