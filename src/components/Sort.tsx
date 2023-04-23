8b012032154922881c01d10d829fd185322318b6
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-23 14:35:06 +0100

    sort styling updates

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Sort = (props: Props) => {
   return (
-    <select className='bg-orange-50 text-orange-500 text-sm rounded-full block w-full p-2.5 '>
+    <select className='bg-orange-50 text-orange-500 text-sm rounded-full block w-full p-2.5 outline-none'>
       <option selected>Sort by</option>
       <option value='newest'>Newest</option>
       <option value='oldest'>Oldest</option>
