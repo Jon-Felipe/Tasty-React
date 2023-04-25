608cf246fa5472167bb98164744dcea7d43b9a10
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-25 14:34:45 +0100

    open and close accordion functionality

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
 
 // extras
 import { MealOptionType } from '../utils/types';
@@ -10,22 +10,38 @@ type Props = {
 };
 
 const Accordion = ({ headerText, options }: Props) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
   return (
     <div className='border-b-2 border-b-orange-100 mb-4'>
-      <div className='flex items-center justify-between'>
+      <div
+        className='flex items-center justify-between cursor-pointer'
+        onClick={() => setIsVisible(!isVisible)}
+      >
         <h3 className='text-2xl text-orange-500 font-semibold mb-4'>
           {headerText}
         </h3>
-        <ChevronDownIcon className='h-5 w-5 text-orange-500' />
+        {isVisible ? (
+          <ChevronDownIcon className='h-5 w-5 text-orange-500' />
+        ) : (
+          <ChevronUpIcon className='h-5 w-5 text-orange-500' />
+        )}
       </div>
-      {options.map((option: MealOptionType) => (
-        <div key={option.id} className='flex items-center justify-between mb-4'>
-          <label htmlFor='' className='capitalize font-medium'>
-            {option.text}
-          </label>
-          <input type='checkbox' className='accent-orange-600' />
-        </div>
-      ))}
+      {isVisible && (
+        <>
+          {options.map((option: MealOptionType) => (
+            <div
+              key={option.id}
+              className='flex items-center justify-between mb-4'
+            >
+              <label htmlFor='' className='capitalize font-medium'>
+                {option.text}
+              </label>
+              <input type='checkbox' className='accent-orange-600' />
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
