76dd52918250f3a6a2341d580963305a339270f0
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-25 19:33:39 +0100

    styling updates

--- src/pages/SingleRecipe.tsx
+++ src/pages/SingleRecipe.tsx
@@ -17,14 +17,18 @@ const SingleRecipe = () => {
   const recipe = recipes.find((recipe) => recipe.id === Number(id));
 
   return (
-    <article className='grid lg:grid-cols-5'>
-      <section className='col-start-1 col-end-4'>
-        <img src={foodImg} alt={recipe?.text} />
-        <h1 className='text-xl font-bold mt-4 tracking px-6 lg:px-0'>
-          {recipe?.text}
-        </h1>
-        <article className='flex flex-col lg:flex-row px-6 lg:px-0 lg:justify-between'>
-          <section className={`${flexRow} justify-between py-2 lg:order-2`}>
+    <article className='grid md:grid-cols-7 gap-4 max-w-screen-xl mx-auto'>
+      <section className='col-span-5'>
+        <img
+          src={foodImg}
+          alt={recipe?.text}
+          className='w-full h-2/3 object-cover rounded-lg'
+        />
+        <h1 className='text-xl font-bold mt-2'>{recipe?.text}</h1>
+        <article className='flex flex-col md:flex-row md:justify-between'>
+          <section
+            className={`${flexRow} justify-between md:gap-2 md:order-2 mt-2.5`}
+          >
             <div className='bg-orange-100 px-4 py-2 rounded-lg'>
               <p className='text-xs text-orange-500 font-medium'>
                 {recipe?.reviews} reviews
@@ -53,16 +57,21 @@ const SingleRecipe = () => {
               <HeartIcon className='w-5 h-5 text-orange-500' />
             </div>
           </section>
-          <section className={`${flexRow} justify-between lg:order-1`}>
+          <section
+            className={`${flexRow} justify-between md:gap-2 lg:order-1 mt-2.5`}
+          >
             <div>
               <h3 className='text-sm font-bold'>{recipe?.author}</h3>
               <p className='text-xs text-orange-500'>Followers: 1561</p>
             </div>
-            <button className='border-2 border-orange-500 text-orange-500 text-lg font-semibold rounded-2xl px-4 py-1'>
+            <button className='border-2 border-orange-500 text-orange-500 text-sm font-semibold rounded-xl px-4 py-1'>
               Follow
             </button>
           </section>
         </article>
+      </section>
+      <section className='col-span-2'>
+        <h3>Ingredients</h3>
       </section>
     </article>
   );
