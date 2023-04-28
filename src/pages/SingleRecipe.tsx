c34d79e663372c92d997c992891e18234174fdab
Jon-Felipe <rodriguesjf7@gmail.com>
2023-04-28 09:51:03 +0100

    styling updates

--- src/pages/SingleRecipe.tsx
+++ src/pages/SingleRecipe.tsx
@@ -133,13 +133,14 @@ const SingleRecipe = () => {
               </div>
             </div>
           </article>
+          {/* equipment */}
           <article className='bg-orange-100 px-6 py-4 rounded-lg mt-4'>
             <h1 className='text-3xl font-bold'>Equipment</h1>
             <ul>
               {recipe?.equipment.map((item, index) => (
                 <li
                   key={index}
-                  className='py-2 px-6 bg-white rounded-lg my-4 text-sm font-bold'
+                  className='py-2 px-4 bg-white rounded-lg my-4 text-sm font-bold'
                 >
                   {item}
                 </li>
