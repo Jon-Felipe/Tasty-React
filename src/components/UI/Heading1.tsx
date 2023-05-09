e884fb2c787b03788cb785bac092d1252d5daf5f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-09 11:02:32 +0100

    heading 1 styling

--- src/components/UI/Heading1.tsx
+++ src/components/UI/Heading1.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+  text: string | undefined;
+};
 
-const Heading1 = (props: Props) => {
-  return <div>Heading1</div>;
+const Heading1 = ({ text }: Props) => {
+  return <h1 className='text-2xl md:text-3xl font-bold'>{text}</h1>;
 };
 
 export default Heading1;
