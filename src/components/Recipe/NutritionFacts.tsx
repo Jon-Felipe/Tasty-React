3fbb2bae19be5deea5c6b00e7c85feb6586d1425
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-13 21:21:24 +0100

    separated code into component

--- src/components/Recipe/NutritionFacts.tsx
+++ src/components/Recipe/NutritionFacts.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+  nutritionFacts: {
+    calories: number;
+    fat: number;
+    carbs: number;
+    protein: number;
+  };
+};
 
-const NutritionFacts = (props: Props) => {
-  return <div>NutritionFacts</div>;
+type NutritionalFactsType = {
+  nutriValue: string | number;
+  nutriText: string;
+};
+
+const NutritionFacts = ({ nutritionFacts }: Props) => {
+  const NutritionalItem = ({ nutriValue, nutriText }: NutritionalFactsType) => {
+    return (
+      <div>
+        <h3 className='font-bold'>{nutriValue}</h3>
+        <p className='text-orange-500 font-medium'>{nutriText}</p>
+      </div>
+    );
+  };
+
+  return (
+    <article className='bg-orange-50 rounded-xl py-4 px-8'>
+      <h3 className='text-2xl font-bold'>
+        Nutritional Facts{' '}
+        <span className='text-sm font-medium'>(per serving)</span>
+      </h3>
+      <div className='flex items-center justify-between bg-white px-6 py-2 rounded-xl mt-2'>
+        <NutritionalItem
+          nutriValue={nutritionFacts.calories}
+          nutriText='Calories'
+        />
+        <NutritionalItem
+          nutriValue={`${nutritionFacts.fat}g`}
+          nutriText='Fat'
+        />
+        <NutritionalItem
+          nutriValue={`${nutritionFacts.carbs}g`}
+          nutriText='Carbs'
+        />
+        <NutritionalItem
+          nutriValue={`${nutritionFacts.protein}g`}
+          nutriText='Protein'
+        />
+      </div>
+    </article>
+  );
 };
 
 export default NutritionFacts;
