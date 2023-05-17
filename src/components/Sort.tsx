5deb274d168beeee5a43fe0f608a85ac5745b7ac
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-17 17:25:26 +0100

    fixed select dropdown bug

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Sort = (props: Props) => {
   return (
     <select className='bg-orange-50 text-orange-500 text-sm rounded-full block w-full p-2.5 outline-none'>
-      <option selected>Sort by</option>
+      <option value=''>Sort by</option>
       <option value='newest'>Newest</option>
       <option value='oldest'>Oldest</option>
     </select>
