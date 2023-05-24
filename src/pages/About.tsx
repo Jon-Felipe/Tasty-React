b5b9c62416f35b42ea4d0826189592379fb3a0bc
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-24 20:45:32 +0100

    about page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -37,19 +37,36 @@ const About = (props: Props) => {
             earum, quam quasi animi, sit iure. Blanditiis?
           </p>
           <br />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-            voluptatibus porro et culpa perferendis, vel recusandae qui corrupti
-            voluptatum ipsam nemo eveniet autem voluptas unde minima fuga optio
-            totam fugit veritatis soluta? Voluptate nulla explicabo eaque
-            officia expedita tempore obcaecati. Vitae expedita magnam, ab sunt
-            quibusdam labore odit! Quasi sequi beatae, quas id unde neque vero?
-            Minima facere sapiente culpa fugiat repudiandae, in rem sit dolorum,
-            perspiciatis beatae repellat, quidem deserunt. Repellat incidunt
-            corrupti modi sint doloremque necessitatibus nemo, nobis itaque ipsa
-            vel quasi sunt qui similique tenetur quis architecto veniam, rem
-            provident in. Debitis esse asperiores nemo quo alias!
-          </p>
+          <div className='flex flex-col md:flex-row items-center gap-y-4 md:gap-y-0 gap-x-4'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium voluptatibus porro et culpa perferendis, vel
+              recusandae qui corrupti voluptatum ipsam nemo eveniet autem
+              voluptas unde minima fuga optio totam fugit veritatis soluta?
+              Voluptate nulla explicabo eaque officia expedita tempore
+              obcaecati. Vitae expedita magnam, ab sunt quibusdam labore odit!
+              Quasi sequi beatae, quas id unde neque vero? Minima facere
+              sapiente culpa fugiat repudiandae, in rem sit dolorum,
+              perspiciatis beatae repellat, quidem deserunt. Repellat incidunt
+              corrupti modi sint doloremque necessitatibus nemo, nobis itaque
+              ipsa vel quasi sunt qui similique tenetur quis architecto veniam,
+              rem provident in. Debitis esse asperiores nemo quo alias!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium voluptatibus porro et culpa perferendis, vel
+              recusandae qui corrupti voluptatum ipsam nemo eveniet autem
+              voluptas unde minima fuga optio totam fugit veritatis soluta?
+              Voluptate nulla explicabo eaque officia expedita tempore
+              obcaecati. Vitae expedita magnam, ab sunt quibusdam labore odit!
+              Quasi sequi beatae, quas id unde neque vero? Minima facere
+              sapiente culpa fugiat repudiandae, in rem sit dolorum,
+              perspiciatis beatae repellat, quidem deserunt. Repellat incidunt
+              corrupti modi sint doloremque necessitatibus nemo, nobis itaque
+              ipsa vel quasi sunt qui similique tenetur quis architecto veniam,
+              rem provident in. Debitis esse asperiores nemo quo alias!
+            </p>
+          </div>
         </article>
       </section>
       <section className='mt-4'>
