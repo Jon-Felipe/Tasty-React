2a9eaf99ea7cbe890877e3a5a5ce51b7ef63a47c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-26 14:22:01 +0100

    disabling login button on loading

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,7 +21,7 @@ const Login = (props: Props) => {
 
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
-  const { user } = useSelector((store: RootState) => store.user);
+  const { user, isLoading } = useSelector((store: RootState) => store.user);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const name = e.target.name;
@@ -81,9 +81,10 @@ const Login = (props: Props) => {
               />
               <button
                 type='submit'
-                className='w-full text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+                className='w-full text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-primary-400'
+                disabled={isLoading}
               >
-                Sign in
+                {isLoading ? 'Loading...' : 'Sign in'}
               </button>
               <p className='text-sm font-light text-gray-500'>
                 Don't have an account yet?{' '}
