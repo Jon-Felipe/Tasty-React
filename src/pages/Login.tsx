369760a6b7e6ac7274ecfc314a1886c8ff0cb62e
Jon-Felipe <rodriguesjf7@gmail.com>
2023-05-29 22:45:44 +0100

    styling updates

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -48,7 +48,7 @@ const Login = () => {
 
   return (
     <section>
-      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+      <div className='flex flex-col items-center justify-center px-6 py-4 mx-auto lg:py-0'>
         <Link
           to='/'
           className='flex items-center mb-6 text-2xl font-semibold text-gray-900'
@@ -57,7 +57,7 @@ const Login = () => {
         </Link>
         <div className='w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 '>
           <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900'>
               Sign in to your account
             </h1>
             <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
