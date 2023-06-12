bc0ab7b9daf64f89adf7ec4360e7818ee8404cda
Jon-Felipe <rodriguesjf7@gmail.com>
2023-06-12 08:47:52 +0100

    displaying link button container in profile screen

--- src/components/Profile/LinkButtonContainer.tsx
+++ src/components/Profile/LinkButtonContainer.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import ProfileLinkButton from './ProfileLinkButton';
 
 type Props = {};
 
 const LinkButtonContainer = (props: Props) => {
-  return <div>LinkButtonContainer</div>;
+  return (
+    <section className='basis-1/4 space-y-4'>
+      <ProfileLinkButton
+        text='My Information'
+        onClick={() => console.log('info')}
+      />
+      <ProfileLinkButton
+        text='My Recipes'
+        onClick={() => console.log('my recipes')}
+      />
+      <button
+        className='block w-full border p-4 shadow rounded bg-red-500 text-white'
+        onClick={() => console.log('delete')}
+      >
+        Delete Account
+      </button>
+    </section>
+  );
 };
 
 export default LinkButtonContainer;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { logoutUser } from '../features/user/userSlice';
+import { useState } from 'react';
 
 // components
 import UpdateProfileForm from '../components/Profile/UpdateProfileForm';
-import ProfileLinkButton from '../components/Profile/ProfileLinkButton';
 import MyRecipes from '../components/Profile/MyRecipes';
 import FavouriteRecipes from '../components/Profile/FavouriteRecipes';
+import LinkButtonContainer from '../components/Profile/LinkButtonContainer';
 
 const Profile = () => {
   const [link, setLink] = useState<string>('info');
-
-  const dispatch = useDispatch();
-
-  const handleOnClick = (value: string) => {
-    setLink(value);
-  };
 
   return (
     <article className='max-w-screen-xl mx-auto py-2'>
@@ -24,26 +16,7 @@ const Profile = () => {
         Details about your Personal Information
       </p>
       <div className='flex flex-col md:flex-row gap-4 mt-2 md:mt-4'>
-        <section className='basis-1/4 space-y-4'>
-          <ProfileLinkButton
-            text='My Information'
-            onClick={() => handleOnClick('info')}
-          />
-          <ProfileLinkButton
-            text='My Recipes'
-            onClick={() => handleOnClick('myRecipes')}
-          />
-          <ProfileLinkButton
-            text='Favourited Recipes'
-            onClick={() => handleOnClick('favouriteRecipes')}
-          />
-          <button
-            className='block w-full border p-4 shadow rounded bg-red-500 text-white'
-            onClick={() => dispatch(logoutUser())}
-          >
-            Delete Account
-          </button>
-        </section>
+        <LinkButtonContainer />
         {link === 'info' ? (
           <UpdateProfileForm />
         ) : link === 'myRecipes' ? (
