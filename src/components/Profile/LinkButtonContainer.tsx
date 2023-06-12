f36980452a30cfe8a6b737031641e0a2297dfa76
Jon-Felipe <rodriguesjf7@gmail.com>
2023-06-12 08:44:16 +0100

    created link button container component

---
+++ src/components/Profile/LinkButtonContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const LinkButtonContainer = (props: Props) => {
+  return <div>LinkButtonContainer</div>;
+};
+
+export default LinkButtonContainer;
