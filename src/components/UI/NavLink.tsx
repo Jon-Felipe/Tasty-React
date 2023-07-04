f5aca0a772260a77236487ee796da2795d1a3d0e
Jon-Felipe <rodriguesjf7@gmail.com>
2023-07-04 21:09:10 +0100

    link colour change

--- src/components/UI/NavLink.tsx
+++ src/components/UI/NavLink.tsx
@@ -11,7 +11,7 @@ const NavLink = ({ text, path }: Props) => {
     <li>
       <Link
         to={path}
-        className='block py-2 pr-4 pl-3 text-gray-700 capitalize border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0'
+        className='block py-2 pr-4 pl-3 text-gray-700 capitalize border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-500 lg:p-0'
       >
         {text}
       </Link>
