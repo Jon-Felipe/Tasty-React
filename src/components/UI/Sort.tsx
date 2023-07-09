a3468b3ca058ccc0a6ce237918a52ec10c905a2c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-07-09 09:40:55 +0100

    added more sort options

--- src/components/UI/Sort.tsx
+++ src/components/UI/Sort.tsx
@@ -5,9 +5,13 @@ type Props = {};
 const Sort = (props: Props) => {
   return (
     <select className='bg-orange-50 text-orange-500 text-sm rounded-full block w-full p-2.5 outline-none'>
-      <option value=''>Sort by</option>
-      <option value='newest'>Newest</option>
+      <option value='' disabled>
+        Sort By:
+      </option>
+      <option value='latest'>Latest</option>
       <option value='oldest'>Oldest</option>
+      <option value='a-z'>Name: A-Z</option>
+      <option value='z-a'>Name: Z-A</option>
     </select>
   );
 };
