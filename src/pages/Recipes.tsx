574a7f42544dc09524d940d9eb86faa3166a81f6
Jon-Felipe <rodriguesjf7@gmail.com>
2023-07-17 18:24:16 +0100

    displaying recipes based on category

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../store';
+import { getRecipesByCategory } from '../features/allRecipes/recipeSlice';
+import RecipeList from '../components/RecipeList';
 
 type Props = {};
 
 const Recipes = (props: Props) => {
-  return <div>Recipes</div>;
+  const params = useParams();
+  const dispatch = useDispatch<AppDispatch>();
+  const { recipes } = useSelector((state: RootState) => state.recipe);
+
+  useEffect(() => {
+    dispatch(getRecipesByCategory(params?.category!));
+  }, []);
+
+  return <RecipeList recipes={recipes} />;
 };
 
 export default Recipes;
