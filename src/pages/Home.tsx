5de3609e1640e12973b940fd200126135db52f80
Jon-Felipe <rodriguesjf7@gmail.com>
2023-07-29 17:06:44 +0100

    removed recipes category and link card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
-import {
-  getAllRecipes,
-  handleChange,
-} from '../features/allRecipes/recipeSlice';
+import { getAllRecipes } from '../features/allRecipes/recipeSlice';
 
 // components
 import RecipeList from '../components/RecipeList';
 import Spinner from '../components/UI/Spinner';
-import LinkCard from '../components/UI/LinkCard';
-import NotFound from '../components/NotFound';
 
 // extras
-import { recipeCategories } from '../utils/constants';
 import heroImg from '../assets/hero-img.jpg';
 
 const Home = () => {
@@ -46,21 +40,6 @@ const Home = () => {
             Over 500 recipes to enjoy
           </p>
         </section>
-      </article>
-      {/* recipe categories */}
-      <article>
-        <h3 className='text-3xl font-bold text-orange-500 tracking-wide underline mb-4'>
-          Popular Categories
-        </h3>
-        <div className='grid gap-y-4 md:grid-cols-2 lg:grid-cols-4 md:gap-4'>
-          {recipeCategories.map((category) => (
-            <LinkCard
-              key={category.id}
-              url={`/recipes/${category.text}`}
-              text={`${category.text} recipes`}
-            />
-          ))}
-        </div>
       </article>
       <article>
         {isLoading ? (
