c814d71bd2e410559c22d5d750c9a8bc5d9cd92a
Jon-Felipe <rodriguesjf7@gmail.com>
2023-07-29 17:18:49 +0100

    renamed component

---
+++ src/components/DiscoverList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const DiscoverList = (props: Props) => {
+  return <div>DiscoverList</div>;
+};
+
+export default DiscoverList;
--- src/components/DiscoveryList.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const DiscoveryList = (props: Props) => {
-  return <div>DiscoveryList</div>;
-};
-
-export default DiscoveryList;
