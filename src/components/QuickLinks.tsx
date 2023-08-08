a2d3816dd4abcbb1d0167ba6e46fb9fcba680500
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-08 15:37:04 +0100

    refactored recipe quick links to quick links component

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
+// extras
+import { recipesQuickLink } from '../utils/constants';
+import allIcon from '../assets/icons/all-icon.png';
 
 type Props = {};
 
 const QuickLinks = (props: Props) => {
-  return <div>QuickLinks</div>;
+  return (
+    <section className='grid md:grid-cols-3 lg:grid-cols-9 gap-y-2 md:gap-4 lg:gap-y-0'>
+      {recipesQuickLink.map(({ id, text, path, icon }) => (
+        <RecipeLink id={id} text={text} path={path} icon={icon} />
+      ))}
+      <RecipeLink text='Browse All' path='recipes' icon={allIcon} />
+    </section>
+  );
 };
 
 export default QuickLinks;
+
+type LinkProps = {
+  id?: number;
+  path: string;
+  icon: string;
+  text: string;
+};
+
+const RecipeLink = ({ id, path, icon, text }: LinkProps) => {
+  return (
+    <article className='border-2 rounded-xl text-center p-2'>
+      <Link key={id} to={`category/${path}`}>
+        <img
+          src={icon}
+          alt={text}
+          className='hidden md:block mb-2 md:mx-auto'
+        />
+        <p className='font-bold lg:text-xs'>{text}</p>
+      </Link>
+    </article>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,8 @@ import Spinner from '../components/Spinner';
 
 // extras
 import heroImg from '../assets/hero-img.jpg';
-import allIcon from '../assets/icons/all-icon.png';
 import { recipesQuickLink } from '../utils/constants';
+import QuickLinks from '../components/QuickLinks';
 
 const Home = () => {
   // const dispatch = useDispatch<AppDispatch>();
@@ -48,30 +48,7 @@ const Home = () => {
         </section>
       </article>
       {/* recipes quick links */}
-      <section className='grid md:grid-cols-3 lg:grid-cols-9 gap-y-2 md:gap-4 lg:gap-y-0'>
-        {recipesQuickLink.map(({ id, text, path, icon }) => (
-          <Link key={id} to={`category/${path}`}>
-            <div className='border-2 rounded-xl text-center p-2'>
-              <img
-                src={icon}
-                alt={text}
-                className='hidden md:block mb-2 md:mx-auto'
-              />
-              <p className='font-bold lg:text-xs'>{text}</p>
-            </div>
-          </Link>
-        ))}
-        <Link to={'/recipes'}>
-          <div className='border-2 rounded-xl text-center p-2'>
-            <img
-              src={allIcon}
-              alt='arrow-up'
-              className='hidden md:block mb-2 md:mx-auto'
-            />
-            <p className='font-bold lg:text-xs'>Browse All</p>
-          </div>
-        </Link>
-      </section>
+      <QuickLinks />
     </div>
   );
 };
