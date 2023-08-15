e87b11af867078ceee19ce62e65b1e5e4698bb2b
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-15 14:01:20 +0100

    recipe list styling change

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const RecipeList = ({ recipes }: Props) => {
   return (
-    <section className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+    <section className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
       {recipes.map((recipe) => {
         return (
           <Recipe
