f0101a9df6a12c341a5681dd0a7e667374f42f3d
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-15 12:26:51 +0100

    removed id param from links props

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -9,7 +9,7 @@ const QuickLinks = (props: Props) => {
   return (
     <section className='grid md:grid-cols-3 lg:grid-cols-9 gap-y-2 md:gap-4 lg:gap-y-0'>
       {recipesQuickLink.map(({ id, text, path, icon }) => (
-        <RecipeLink key={id} id={id} text={text} path={path} icon={icon} />
+        <RecipeLink key={id} text={text} path={path} icon={icon} />
       ))}
     </section>
   );
@@ -18,13 +18,12 @@ const QuickLinks = (props: Props) => {
 export default QuickLinks;
 
 type LinkProps = {
-  id: number;
   path: string;
   icon: string;
   text: string;
 };
 
-const RecipeLink = ({ id, path, icon, text }: LinkProps) => {
+const RecipeLink = ({ path, icon, text }: LinkProps) => {
   return (
     <article className='border-2 rounded-xl text-center p-2'>
       <Link to={path}>
