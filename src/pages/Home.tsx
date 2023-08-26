3494b1fd8d91a5ee219ba3949722f47f9e64b2fe
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-26 21:42:45 +0100

    refactored home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,40 +5,36 @@ import { getAllRecipes } from '../features/allRecipes/recipeSlice';
 
 // components
 import Spinner from '../components/Spinner';
-import CategoryList from '../components/CategoryList';
-import QuickLink from '../components/QuickLink';
 
 // extras
 import heroImg from '../assets/hero-img.jpg';
-import { getRecipesByCategory } from '../utils/helpers';
-import { recipesQuickLink } from '../utils/constants';
+import PageButtonContainer from '../components/PageButtonContainer';
 
 const Home = () => {
   const dispatch = useDispatch<AppDispatch>();
 
-  const { isLoading, recipes } = useSelector(
+  const { isLoading, recipes, page } = useSelector(
     (state: RootState) => state.recipe
   );
 
   useEffect(() => {
-    dispatch(getAllRecipes({ cuisine: ['european', 'african', 'thai'] }));
-  }, []);
+    dispatch(getAllRecipes({ limit: 12, page }));
+  }, [page]);
 
   if (isLoading) {
     return <Spinner />;
   }
 
   return (
-    <div className='grid gap-6'>
-      {/* hero */}
-      <article className='relative'>
-        <section className='h-52 lg:h-96 rounded-xl overflow-hidden'>
+    <>
+      <section className='relative'>
+        <article className='h-52 lg:h-96 rounded-xl overflow-hidden'>
           <img
             src={heroImg}
             alt='hero image'
             className='w-full h-full object-cover'
           />
-        </section>
+        </article>
         <section className='absolute top-2 lg:top-4 left-6 lg:left-6 w-2/4 lg:w-2/5'>
           <h3 className='text-lg lg:text-5xl text-white font-semibold capitalize'>
             Cook up a storm and take all the credit
@@ -47,30 +43,24 @@ const Home = () => {
             Over 500 recipes to enjoy
           </p>
         </section>
-      </article>
-      {/* recipes quick links */}
-      <section className='grid md:grid-cols-3 lg:grid-cols-9 gap-y-2 md:gap-4 lg:gap-y-0'>
-        {recipesQuickLink.map(({ id, path, icon, text }) => (
-          <QuickLink key={id} path={path} icon={icon} text={text} />
-        ))}
       </section>
-      {/* <QuickLinks /> */}
-      {/* european recipes category list */}
-      <CategoryList
-        categoyTitle='european'
-        recipes={getRecipesByCategory(recipes, 'european')}
-      />
-      {/* african recipes category list */}
-      <CategoryList
-        categoyTitle='african'
-        recipes={getRecipesByCategory(recipes, 'african')}
-      />
-      {/* thai recipes category list */}
-      <CategoryList
-        categoyTitle='thai'
-        recipes={getRecipesByCategory(recipes, 'thai')}
-      />
-    </div>
+      <section className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-4 mt-6'>
+        {recipes.map((recipe) => {
+          return (
+            <article key={recipe._id}>
+              <img src={heroImg} alt={recipe.image} />
+              <div className='mt-1'>
+                <h3 className='text-2xl font-bold'>{recipe.name}</h3>
+                <p className='text-sm tracking-tight'>
+                  {recipe.description.slice(0, 80)}...
+                </p>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+      <PageButtonContainer />
+    </>
   );
 };
 
