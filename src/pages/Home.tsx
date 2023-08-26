09741151d8a6effdcb086cd12ad4a5656cb29bd8
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-26 21:47:10 +0100

    changed hero html tags

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,21 +29,21 @@ const Home = () => {
   return (
     <>
       <section className='relative'>
-        <article className='h-52 lg:h-96 rounded-xl overflow-hidden'>
+        <div className='h-52 lg:h-96 rounded-xl overflow-hidden'>
           <img
             src={heroImg}
             alt='hero image'
             className='w-full h-full object-cover'
           />
-        </article>
-        <section className='absolute top-2 lg:top-4 left-6 lg:left-6 w-2/4 lg:w-2/5'>
+        </div>
+        <article className='absolute top-2 lg:top-4 left-6 lg:left-6 w-2/4 lg:w-2/5'>
           <h3 className='text-lg lg:text-5xl text-white font-semibold capitalize'>
             Cook up a storm and take all the credit
           </h3>
           <p className='text-sm lg:text-2xl text-white lg:pt-2.5'>
             Over 500 recipes to enjoy
           </p>
-        </section>
+        </article>
       </section>
       <section className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-4 mt-6'>
         {recipes.map((recipe) => {
