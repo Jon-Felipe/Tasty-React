014136f80b5d0994f066b7342adde5e93b6d3bf8
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-27 13:11:32 +0100

    cuisine and dish type accordion filters

--- src/pages/RecipeSearch.tsx
+++ src/pages/RecipeSearch.tsx
@@ -4,6 +4,8 @@ import { AppDispatch, RootState } from '../store';
 import { getAllRecipes } from '../features/allRecipes/recipeSlice';
 import RecipeCard from '../components/RecipeCard';
 import Search from '../components/Search';
+import Accordion from '../components/Accordion';
+import { cuisineTypes, dishTypes } from '../utils/constants';
 
 const RecipeSearch = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -15,13 +17,27 @@ const RecipeSearch = () => {
 
   return (
     <section>
-      <h1 className='text-5xl font-bold uppercase underline'>Recipe Search</h1>
+      <h1 className='text-4xl font-bold uppercase underline'>Recipe Search</h1>
       <div className='grid lg:grid-cols-[350px_1fr] gap-y-4 lg:gap-x-4 mt-8'>
-        <Search
-          value=''
-          onChange={() => console.log('')}
-          onSubmit={() => console.log('')}
-        />
+        <section className='space-y-4'>
+          <Search
+            value=''
+            onChange={() => console.log('')}
+            onSubmit={() => console.log('')}
+          />
+          <Accordion
+            headerText='Cuisines'
+            name='cuisines'
+            checkedValue=''
+            options={cuisineTypes}
+          />
+          <Accordion
+            headerText='Dishes'
+            name='dishType'
+            checkedValue=''
+            options={dishTypes}
+          />
+        </section>
         <section className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-3'>
           {recipes.map((recipe) => {
             return <RecipeCard key={recipe._id} recipe={recipe} />;
