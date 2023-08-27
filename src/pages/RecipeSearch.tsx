ef9ba455395d0d314318cb97921193318a587909
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-27 19:26:45 +0100

    filter clear and search button

--- src/pages/RecipeSearch.tsx
+++ src/pages/RecipeSearch.tsx
@@ -45,6 +45,14 @@ const RecipeSearch = () => {
             name='dishType'
             options={dishTypes}
           /> */}
+          <div className='flex items-center gap-x-4'>
+            <button className='bg-white text-slate-700 border-2 border-slate-700 text-xs font-bold uppercase px-4 py-1.5 rounded-lg hover:bg-slate-700 hover:text-white'>
+              Clear
+            </button>
+            <button className='bg-orange-400 text-white border-2 border-orange-400 text-xs font-bold uppercase px-4 py-1.5 rounded-lg hover:bg-white hover:text-orange-400'>
+              Search
+            </button>
+          </div>
         </section>
         <section className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-3'>
           {recipes.map((recipe) => {
