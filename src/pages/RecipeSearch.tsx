db4fd1c4fae1ae2409b6087e50f0c0db0637829c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-08-27 13:05:38 +0100

    search filter display

--- src/pages/RecipeSearch.tsx
+++ src/pages/RecipeSearch.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
 import { getAllRecipes } from '../features/allRecipes/recipeSlice';
 import RecipeCard from '../components/RecipeCard';
+import Search from '../components/Search';
 
 const RecipeSearch = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -15,16 +16,18 @@ const RecipeSearch = () => {
   return (
     <section>
       <h1 className='text-5xl font-bold uppercase underline'>Recipe Search</h1>
-      <section className='grid lg:grid-cols-[250px_1fr] mt-8'>
-        <div>
-          <h3 className='text-xl font-bold'>Filters</h3>
-        </div>
+      <div className='grid lg:grid-cols-[350px_1fr] gap-y-4 lg:gap-x-4 mt-8'>
+        <Search
+          value=''
+          onChange={() => console.log('')}
+          onSubmit={() => console.log('')}
+        />
         <section className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-3'>
           {recipes.map((recipe) => {
             return <RecipeCard key={recipe._id} recipe={recipe} />;
           })}
         </section>
-      </section>
+      </div>
     </section>
   );
 };
