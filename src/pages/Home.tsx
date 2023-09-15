0e0a41c34101e3688522cdedabd6c0a4aa665c21
Jon-Felipe <rodriguesjf7@gmail.com>
2023-09-15 10:29:19 +0100

    featured breakfast list home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,7 @@ import {
 import Spinner from '../components/Spinner';
 import RecipeCard from '../components/RecipeCard';
 import { Link } from 'react-router-dom';
+import FeaturedList from '../components/FeaturedList';
 
 const Home = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -65,17 +66,8 @@ const Home = () => {
           </Link>
         </div>
       </section>
-      {/* popular recipes section */}
-      <section className='mt-6'>
-        <h3 className='text-2xl md:text-4xl text-slate-800 font-bold mb-4 underline'>
-          Popular Recipes
-        </h3>
-        <div className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-4'>
-          {recipes.map((recipe) => {
-            return <RecipeCard key={recipe._id} recipe={recipe} />;
-          })}
-        </div>
-      </section>
+      {/* breakfast recipes list section */}
+      <FeaturedList title='Favourite Breakfast Ideas' recipes={recipes} />
     </>
   );
 };
