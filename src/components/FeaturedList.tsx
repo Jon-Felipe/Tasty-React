40f158f7407ceb565d0c2af8f902286fbda83280
Jon-Felipe <rodriguesjf7@gmail.com>
2023-09-15 10:23:27 +0100

    featured list styling and displaying recipes

--- src/components/FeaturedList.tsx
+++ src/components/FeaturedList.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+// components
+import RecipeCard from './RecipeCard';
+// extras
+import { SingleRecipeType } from '../utils/types';
 
-type Props = {};
+type Props = {
+  title: string;
+  recipes: SingleRecipeType[];
+};
 
-const FeaturedList = (props: Props) => {
-  return <div>FeaturedList</div>;
+const FeaturedList = ({ title, recipes }: Props) => {
+  return (
+    <section className='mt-6'>
+      <h3 className='text-2xl md:text-4xl text-slate-800 font-bold mb-4 underline'>
+        {title}
+      </h3>
+      <div className='grid gap-y-6 md:gap-x-6 md:grid-cols-2 lg:grid-cols-4'>
+        {recipes.map((recipe) => {
+          return <RecipeCard key={recipe._id} recipe={recipe} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedList;
