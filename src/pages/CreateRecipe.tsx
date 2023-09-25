a2388705eddb51292a4d4d8e86e613c2574dcda2
Jon-Felipe <rodriguesjf7@gmail.com>
2023-09-25 08:58:24 +0100

    removed form styling create recipe page

--- src/pages/CreateRecipe.tsx
+++ src/pages/CreateRecipe.tsx
@@ -7,160 +7,150 @@ import FormRowSelect from '../components/FormRowSelect';
 const CreateRecipe = () => {
   return (
     <section>
-      <header>
-        <h1 className='text-3xl font-semibold tracking-wide'>Create Recipe</h1>
-      </header>
-      <article className='p-8 shadow rounded-md mt-4'>
-        <form className='flex flex-col md:flex-row gap-y-4 md:gap-y-0 md:gap-x-4'>
-          <div className='w-full'>
-            {/* recipe image */}
-            <FormRow
-              name='image'
-              labelText='Image'
-              type='file'
-              value={''}
-              handleChange={() => console.log('name')}
-              required
-            />
-            {/* recipe name */}
-            <FormRow
-              name='name'
-              labelText='Name'
-              type='text'
-              placeholder='E.g. Lemo Garlic Shrimp'
-              value={''}
-              handleChange={() => console.log('name')}
-              required
-            />
-            {/* recipe description */}
-            <div className='mt-2'>
-              <label
-                htmlFor='description'
-                className='block mb-2 text-sm font-medium text-gray-900'
-              >
-                Description
-              </label>
-              <textarea
-                name='description'
-                id='description'
-                value={''}
-                onChange={() => console.log('description')}
-                placeholder='Add your description here'
-                className='border rounded p-2 w-full h-32'
-              />
-            </div>
-            <div className='flex flex-col md:flex-row md:items-center md:justify-between gap-y-4 md:gap-x-4 mt-2'>
-              {/* prep time */}
-              <FormRow
-                name='prepTime'
-                labelText='Prep Time'
-                type='number'
-                placeholder='E.g. 15'
-                value={''}
-                handleChange={() => console.log('prepTime')}
-                required
-              />
-              {/* cook time */}
-              <FormRow
-                name='cookTime'
-                labelText='Cook Time'
-                type='number'
-                placeholder='E.g. 45'
-                value={''}
-                handleChange={() => console.log('cookTime')}
-                required
-              />
-              {/* servings */}
-              <FormRow
-                name='servings'
-                labelText='Servings'
-                type='number'
-                placeholder='E.g. 6'
-                value={''}
-                handleChange={() => console.log('servings')}
-                required
-              />
-            </div>
-            <div className='flex flex-col md:flex-row md:items-center md:justify-between gap-y-4 md:gap-x-4 mt-2'>
-              {/* difficulty */}
-              <FormRowSelect
-                labelText='Difficulty'
-                name='difficulty'
-                value={''}
-                onChange={() => console.log('difficulty')}
-                list={[
-                  { id: 1, listItem: 'Easy' },
-                  { id: 2, listItem: 'Medium' },
-                ]}
-              />
-              {/* cuisine */}
-              <FormRowSelect
-                labelText='Cuisine'
-                name='cuisine'
-                value={''}
-                onChange={() => console.log('cuisine')}
-                list={[
-                  { id: 1, listItem: 'european' },
-                  { id: 2, listItem: 'asian' },
-                ]}
-              />
-              {/* dishType */}
-              <FormRowSelect
-                labelText='Dish Type'
-                name='dishType'
-                value={''}
-                onChange={() => console.log('dishType')}
-                list={[
-                  { id: 1, listItem: 'breakfast' },
-                  { id: 2, listItem: 'lunch' },
-                ]}
-              />
-            </div>
-          </div>
-          <div className='w-full space-y-2'>
-            {/* ingredients */}
-            <AddRecipeItemInputRow
-              name='ingredients'
-              labelText='Ingredients'
-              type='text'
-              placeholder='E.g. 4 Tomatoes'
-              value={''}
-              handleChange={() => console.log('servings')}
-              required
-            />
-            {/* instructions */}
-            <AddRecipeItemInputRow
-              name='instructions'
-              labelText='Instructions'
-              type='text'
-              placeholder='E.g. Mix together the ingredients in one bowl'
-              value={''}
-              handleChange={() => console.log('instructions')}
-              required
-            />
-            {/* equipment */}
-            <AddRecipeItemInputRow
-              name='equipment'
-              labelText='Equipment'
-              type='text'
-              placeholder='E.g. 1 Baking Tray'
-              value={''}
-              handleChange={() => console.log('equipment')}
-              required
-            />
-            {/* tips */}
-            <AddRecipeItemInputRow
-              name='tips'
-              labelText='Tips'
-              type='text'
-              placeholder='E.g. Preheat oven 15 minutes before starting'
-              value={''}
-              handleChange={() => console.log('tips')}
-              required
-            />
-          </div>
-        </form>
-      </article>
+      <h1 className='text-3xl font-semibold tracking-wide mb-4'>
+        Create Recipe
+      </h1>
+      <form>
+        {/* recipe image */}
+        <FormRow
+          name='image'
+          labelText='Image'
+          type='file'
+          value={''}
+          handleChange={() => console.log('name')}
+          required
+        />
+        {/* recipe name */}
+        <FormRow
+          name='name'
+          labelText='Name'
+          type='text'
+          placeholder='E.g. Lemo Garlic Shrimp'
+          value={''}
+          handleChange={() => console.log('name')}
+          required
+        />
+        {/* recipe description */}
+        <div>
+          <label
+            htmlFor='description'
+            className='block mb-2 text-sm font-medium text-gray-900'
+          >
+            Description
+          </label>
+          <textarea
+            name='description'
+            id='description'
+            value={''}
+            onChange={() => console.log('description')}
+            placeholder='Add your description here'
+            className='border rounded p-2 w-full h-32'
+          />
+        </div>
+        {/* prep time */}
+        <FormRow
+          name='prepTime'
+          labelText='Prep Time'
+          type='number'
+          placeholder='E.g. 15'
+          value={''}
+          handleChange={() => console.log('prepTime')}
+          required
+        />
+        {/* cook time */}
+        <FormRow
+          name='cookTime'
+          labelText='Cook Time'
+          type='number'
+          placeholder='E.g. 45'
+          value={''}
+          handleChange={() => console.log('cookTime')}
+          required
+        />
+        {/* servings */}
+        <FormRow
+          name='servings'
+          labelText='Servings'
+          type='number'
+          placeholder='E.g. 6'
+          value={''}
+          handleChange={() => console.log('servings')}
+          required
+        />
+        {/* difficulty */}
+        <FormRowSelect
+          labelText='Difficulty'
+          name='difficulty'
+          value={''}
+          onChange={() => console.log('difficulty')}
+          list={[
+            { id: 1, listItem: 'Easy' },
+            { id: 2, listItem: 'Medium' },
+          ]}
+        />
+        {/* cuisine */}
+        <FormRowSelect
+          labelText='Cuisine'
+          name='cuisine'
+          value={''}
+          onChange={() => console.log('cuisine')}
+          list={[
+            { id: 1, listItem: 'european' },
+            { id: 2, listItem: 'asian' },
+          ]}
+        />
+        {/* dishType */}
+        <FormRowSelect
+          labelText='Dish Type'
+          name='dishType'
+          value={''}
+          onChange={() => console.log('dishType')}
+          list={[
+            { id: 1, listItem: 'breakfast' },
+            { id: 2, listItem: 'lunch' },
+          ]}
+        />
+        {/* ingredients */}
+        <AddRecipeItemInputRow
+          name='ingredients'
+          labelText='Ingredients'
+          type='text'
+          placeholder='E.g. 4 Tomatoes'
+          value={''}
+          handleChange={() => console.log('servings')}
+          required
+        />
+        {/* instructions */}
+        <AddRecipeItemInputRow
+          name='instructions'
+          labelText='Instructions'
+          type='text'
+          placeholder='E.g. Mix together the ingredients in one bowl'
+          value={''}
+          handleChange={() => console.log('instructions')}
+          required
+        />
+        {/* equipment */}
+        <AddRecipeItemInputRow
+          name='equipment'
+          labelText='Equipment'
+          type='text'
+          placeholder='E.g. 1 Baking Tray'
+          value={''}
+          handleChange={() => console.log('equipment')}
+          required
+        />
+        {/* tips */}
+        <AddRecipeItemInputRow
+          name='tips'
+          labelText='Tips'
+          type='text'
+          placeholder='E.g. Preheat oven 15 minutes before starting'
+          value={''}
+          handleChange={() => console.log('tips')}
+          required
+        />
+      </form>
     </section>
   );
 };
