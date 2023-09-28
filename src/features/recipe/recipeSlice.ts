aaa6268e1ae057364e314335ae3cbcf109628efc
Jon-Felipe <rodriguesjf7@gmail.com>
2023-09-28 09:13:52 +0100

    recipe slice initial state

--- src/features/recipe/recipeSlice.ts
+++ src/features/recipe/recipeSlice.ts
@@ -1,6 +1,38 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {};
+interface InitialState {
+  isLoading: boolean;
+  name: string;
+  description: string;
+  prepTime: number;
+  cookTime: number;
+  servings: number;
+  difficulty: string;
+  cuisine: string;
+  dishType: string;
+  ingredients: string[];
+  instructions: string[];
+  equipment: string[];
+  tips: string[];
+  isEditing: boolean;
+}
+
+const initialState: InitialState = {
+  isLoading: false,
+  name: '',
+  description: '',
+  prepTime: 0,
+  cookTime: 0,
+  servings: 0,
+  difficulty: '',
+  cuisine: '',
+  dishType: '',
+  ingredients: [''],
+  instructions: [''],
+  equipment: [''],
+  tips: [''],
+  isEditing: false,
+};
 
 export const recipeSlice = createSlice({
   name: 'recipe',
